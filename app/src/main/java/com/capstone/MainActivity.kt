7b7ec2629249109b3aa5847842dd45a87b508d73
Richard Therrien <richardgtherrien@gmail.com>
2020-03-12 00:49:35 -0400

    added inital geofencing

--- app/src/main/java/com/capstone/MainActivity.kt
+++ app/src/main/java/com/capstone/MainActivity.kt
@@ -1,23 +1,46 @@
 package com.capstone
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.capstone.notifications.MyFirebaseMessagingService
+import com.capstone.notifications.GeofenceBroadcastReceiver
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    private val runningAndroidQOrLater =
+        android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+    private val geofenceRadiusMeters = 100F
+    private lateinit var geofencingClient: GeofencingClient
+    private lateinit var geofenceList: MutableList<Geofence>
 
+    private val geofencePendingIntent: PendingIntent by lazy {
+        val intent = Intent(this, GeofenceBroadcastReceiver::class.java)
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // addGeofences() and removeGeofences().
+        PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +48,8 @@ class MainActivity : AppCompatActivity() {
         MyFirebaseMessagingService().initFirebase(this)
         // create the notification channel
         createChannel()
+        // geofencing
+        geofencingClient = LocationServices.getGeofencingClient(this)
         //check log in status
         val sharedPref = this.getSharedPreferences(
             getString(R.string.shared_preferences_key), Context.MODE_PRIVATE
@@ -53,6 +78,13 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onStart() {
+        super.onStart()
+        checkPermissionsGeofencing()
+    }
+
     // used to crate a notification channel with the visual and auditory behaviours
     private fun createChannel() {
         //create a channel
@@ -76,7 +108,87 @@ class MainActivity : AppCompatActivity() {
                 NotificationManager::class.java
             )
             notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun checkPermissionsGeofencing(){
+        if (locationPermissionApproved()){
+            return
+        }
+        // request permission
+        var permissionsArray = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        val resultCode = when {
+            runningAndroidQOrLater -> {
+                permissionsArray += Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE
+            }
+            else -> REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+        }
+
+        Log.d(TAG, "Request foreground only location permission")
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            permissionsArray,
+            resultCode
+        )
+        // add the geofence
+        addGeofence(43.657738,  -79.377298, "1")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun locationPermissionApproved(): Boolean {
+        val fgPermission = (PackageManager.PERMISSION_GRANTED ==
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION))
+        val bgPermission =
+            if (runningAndroidQOrLater) {
+                PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_BACKGROUND_LOCATION )
+            } else {
+                true
+            }
+        return fgPermission && bgPermission
+    }
+
+    private fun addGeofence(lat: Double, long: Double, index: String) {
+        // Set the request ID of the geofence. This is a string to identify this
+        // geofence.
+        geofenceList.add(
+            Geofence.Builder()
+                .setRequestId(index)
+                .setCircularRegion(lat, long, geofenceRadiusMeters)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER or Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build()
+        )
+        getGeofencingRequest()
+        removeExisitngGeofence()
+    }
+
+    private fun getGeofencingRequest(): GeofencingRequest {
+        return GeofencingRequest.Builder().apply {
+            setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+            addGeofences(geofenceList)
+        }.build()
+    }
+
+    // removes existing geofences that share the pending request
+    private fun removeExisitngGeofence() {
+        geofencingClient?.removeGeofences(geofencePendingIntent)?.run {
+            addOnSuccessListener {
+                Log.d(TAG,"Geofence removed")
+                Toast.makeText(applicationContext, "Geofence removed", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            addOnFailureListener {
+                Log.d(TAG, "Failed to remove geofence")
+                Toast.makeText(applicationContext,"Failed to remove geofence", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 }
+
+private const val REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE = 33
+private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+private const val TAG = "MainActivity"
---
+++ app/src/main/java/com/capstone/notifications/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,64 @@
+package com.capstone.notifications
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingEvent
+
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        if (geofencingEvent.hasError()) {
+            val errorMessage = GeofenceStatusCodes.getStatusCodeString(geofencingEvent.errorCode)
+            Log.e(TAG, errorMessage)
+            return
+        }
+
+        // Get the transition type.
+        val geofenceTransition = geofencingEvent.geofenceTransition
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+        geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            val triggeringGeofences = geofencingEvent.triggeringGeofences
+
+            // Get the transition details as a String.
+            val geofenceTransitionDetails = getGeofenceTransitionDetails(
+                this,
+                geofenceTransition,
+                triggeringGeofences
+            )
+
+            // Send notification and log the transition details.
+            val notificationManager = context!!.getSystemService(
+                NotificationManager::class.java
+            )
+
+            notificationManager?.sendNotification(
+                99999999,
+                "Passed Geo Fence",
+                "The Geofence has been passed",
+                context
+            )
+            Log.i(TAG, geofenceTransitionDetails.toString())
+        } else {
+            Log.e(TAG, "Geofence transition is invalid")
+        }
+    }
+    fun getGeofenceTransitionDetails(broadcastReceiver: GeofenceBroadcastReceiver, transitionType: Int, triggeringGeofences: MutableList<Geofence>){
+
+    }
+
+}
+
+private const val TAG = "GeofenceBroadcastReceiver"
+
